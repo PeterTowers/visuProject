6008ac8045b4aacd14a84b5d5630b86304317204
Pedro Torres <pedrohagatorres@gmail.com>
2021-05-16 14:17:26 -0300

    Added visualizations and some processing to df.

--- app.py
+++ app.py
@@ -9,23 +9,53 @@ import plotly.express as px
 import pandas as pd
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = 'visuProject'
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 
 df = pd.read_csv('data/owid-covid-data.csv')
 df_base = df[df['iso_code'].isin(["BRA", "MEX", "USA", "VNM", "KOR", "CHN", "GBR", "NZL", "AUS", "DEU", "FRA", "SWE"])]
 
-fig = px.scatter(df_base, x='date', y='total_cases', color='location')
+cases = px.line(df_base, x='date', y='total_cases', color='location')
+
+new_cases_smooth = px.line(df_base, x='date', y='new_cases_smoothed', color='location')
+
+deaths = px.line(df_base, x='date', y='total_deaths', color='location')
+
+new_deaths = px.line(df_base, x='date', y='new_deaths_smoothed', color='location')
+
+rep_rate = px.line(df_base, x='date', y='reproduction_rate', color='location')
 
 app.layout = html.Div(children=[
-    html.H1(children='Pitão p/ visú de dados \U0001F919'),
-    html.Div(children='''
-        Smash & Dash: The only way to smash is to then dash \U0001F609
-    '''),
+    html.H1(children='Visualizações básicas'),
+    html.H4(children="Total de casos"),
     dcc.Graph(
-        id='example-graph',
-        figure=fig
+        id='cases',
+        figure=cases
+    ),
+
+    html.H4(children="Média móvel de 7 dias de novos casos"),
+    dcc.Graph(
+        id='new_cases_smooth',
+        figure=new_cases_smooth
+    ),
+
+    html.H4(children="Mortes totais por país"),
+    dcc.Graph(
+        id='deaths',
+        figure=deaths
+    ),
+    html.H4(children="Média móvel de 7 dias de mortes"),
+    dcc.Graph(
+        id='new_deaths',
+        figure=new_deaths
+    ),
+    html.H4(children="Taxa de reprodução (R) do vírus"),
+    dcc.Graph(
+        id='rep_rate',
+        figure=rep_rate
     )
 ])
+
 if __name__ == '__main__':
     app.run_server(debug=True)
