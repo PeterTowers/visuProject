34cd107b1240c9e5a6924c4c5510739a9ee785d5
Pedro Torres <pedrohagatorres@gmail.com>
2021-05-17 18:31:35 -0300

    Refactor: modularizing pages.

--- app.py
+++ app.py
@@ -7,15 +7,15 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_bootstrap_components as dbc
+
+from dash.dependencies import Input, Output
+
 import plotly.graph_objects as go
 import plotly.express as px
 import pandas as pd
 
 # Local imports
-import src.country_clusters as countries
-import src.visualizations as visu
-
-import view.views as views
+from view import index, page_not_found
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css',
                         dbc.themes.DARKLY]
@@ -24,36 +24,25 @@ app.title = 'visuProject'
 # assume you have a "long-form" data frame
 # see https://plotly.com/python/px-arguments/ for more options
 
-app.layout = html.Div(children=[views.navbar, views.landing_text,
-    html.H1(children='Dados gerais globais'),
-    html.H2(children="Total de casos"),
-    dcc.Graph(
-        id='cases',
-        figure=visu.total_cases(countries.general())
-    ),
-
-    html.H2(children="Média móvel de 7 dias de novos casos por milhão de habitantes"),
-    dcc.Graph(
-        id='new_cases_smooth',
-        figure=visu.new_cases_smooth_mil(countries.general())
-    ),
-
-    html.H2(children="Mortes totais por país"),
-    dcc.Graph(
-        id='deaths',
-        figure=visu.total_deaths(countries.general())
-    ),
-    html.H2(children="Média móvel de mortes dos últimos 7 dias por milhão de habitantes"),
-    dcc.Graph(
-        id='new_deaths',
-        figure=visu.new_deaths_smooth_mil(countries.general())
-    ),
-    html.H2(children="Taxa de reprodução (R) do vírus"),
-    dcc.Graph(
-        id='rep_rate',
-        figure=visu.r_rate(countries.general())
-    )
+app.layout = html.Div([
+    dcc.Location(id='url', refresh=False),
+    html.Div(id='page-content')
 ])
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/":
+        return index.index_page()
+    elif pathname == "/page-1":
+        return html.P("This is the content of page 1. Yay!")
+    elif pathname == "/page-2":
+        return html.P("Oh cool, this is page 2!")
+
+    # If the user tries to reach a different page, return a 404 message
+    else:
+        return page_not_found.page_not_found(pathname)
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
