8789c5ee064524792f647fe261bcf911ab5f319a
Pedro Torres <pedrohagatorres@gmail.com>
2021-05-19 16:03:54 -0300

    Styling: added blank line between methods.

--- src/general_purposes_clusters.py
+++ src/general_purposes_clusters.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 df = pd.read_csv('data/owid_ginada.csv')
 
+
 def revenue():
     df_renda = df[['date', 'location', 'continent', 'gdp_per_capita',
                    'life_expectancy', 'hospital_beds_per_thousand',
@@ -9,6 +10,7 @@ def revenue():
     df_renda = df_renda.dropna()
     df_renda = df_renda.drop_duplicates(subset=['location'], keep='last')
     return df_renda
+
 
 def sickness():
     df_comorb = df[['date', 'location', 'continent', 'cardiovasc_death_rate',
